b3d97958a71ebbecf8905e4458c3e93083ef9137
Fabian Diaz <diazllanzaf@gmail.com>
2013-08-18 14:40:27 -0400

    se agrega barra de menu para prueba

--- ColegioEchaurren/ColegioEchaurren/Form1.Designer.vb
+++ ColegioEchaurren/ColegioEchaurren/Form1.Designer.vb
@@ -22,9 +22,100 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.Opción1ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Opción2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Opción1ToolStripMenuItem, Me.Opción2ToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(605, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'Opción1ToolStripMenuItem
+        '
+        Me.Opción1ToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AToolStripMenuItem, Me.BToolStripMenuItem, Me.CToolStripMenuItem})
+        Me.Opción1ToolStripMenuItem.Name = "Opción1ToolStripMenuItem"
+        Me.Opción1ToolStripMenuItem.Size = New System.Drawing.Size(67, 20)
+        Me.Opción1ToolStripMenuItem.Text = "Opción 1"
+        '
+        'Opción2ToolStripMenuItem
+        '
+        Me.Opción2ToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AToolStripMenuItem1, Me.BToolStripMenuItem1, Me.CToolStripMenuItem1})
+        Me.Opción2ToolStripMenuItem.Name = "Opción2ToolStripMenuItem"
+        Me.Opción2ToolStripMenuItem.Size = New System.Drawing.Size(67, 20)
+        Me.Opción2ToolStripMenuItem.Text = "Opción 2"
+        '
+        'AToolStripMenuItem
+        '
+        Me.AToolStripMenuItem.Name = "AToolStripMenuItem"
+        Me.AToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AToolStripMenuItem.Text = "a"
+        '
+        'BToolStripMenuItem
+        '
+        Me.BToolStripMenuItem.Name = "BToolStripMenuItem"
+        Me.BToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.BToolStripMenuItem.Text = "b"
+        '
+        'CToolStripMenuItem
+        '
+        Me.CToolStripMenuItem.Name = "CToolStripMenuItem"
+        Me.CToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.CToolStripMenuItem.Text = "c"
+        '
+        'AToolStripMenuItem1
+        '
+        Me.AToolStripMenuItem1.Name = "AToolStripMenuItem1"
+        Me.AToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.AToolStripMenuItem1.Text = "a"
+        '
+        'BToolStripMenuItem1
+        '
+        Me.BToolStripMenuItem1.Name = "BToolStripMenuItem1"
+        Me.BToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.BToolStripMenuItem1.Text = "b"
+        '
+        'CToolStripMenuItem1
+        '
+        Me.CToolStripMenuItem1.Name = "CToolStripMenuItem1"
+        Me.CToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.CToolStripMenuItem1.Text = "c"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(605, 319)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Colegio Echaurren"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents Opción1ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents BToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Opción2ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents BToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
 
 End Class
