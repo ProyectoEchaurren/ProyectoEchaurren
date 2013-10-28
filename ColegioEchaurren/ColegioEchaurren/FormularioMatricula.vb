961919dc0ed3f3a597503625f0e71fc14d620696
mcabasv <davis.moises@gmail.com>
2013-10-28 19:44:14 -0300

    mas cambios

--- ColegioEchaurren/ColegioEchaurren/FormularioMatricula.vb
+++ ColegioEchaurren/ColegioEchaurren/FormularioMatricula.vb
@@ -4,6 +4,7 @@ Public Class FormularioMatricula
     Public indiceTab As Integer
     Public varConexion As MySqlConnection
     Public varConexionString As String = "server=localhost;User Id=root;password=123456;database=bd_echaurren"
+    Public consultaCargaCombo As String = "SELECT * FROM bd_echaurren.servicio_salud;"
 
     Private Sub FormularioMatricula_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
@@ -24,9 +25,22 @@ Public Class FormularioMatricula
 
         End Try
 
+
+        Try
+            Dim _dataAdapter = New MySqlDataAdapter(consultaCargaCombo, varConexion)
+            Dim _dataSet = New DataSet
+            _dataAdapter.Fill(_dataSet)
+
+            comboServSalud.DataSource = _dataSet.Tables(0)
+            comboServSalud.ValueMember = "idServicio_salud"
+            comboServSalud.DisplayMember = "PlanSalud"
+        Catch ex As Exception
+            MessageBox.Show("Error al cargar servicios de salud", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
     End Sub
 
-    Private Sub ComboBox1_SelectedIndexChanged_1(sender As System.Object, e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+    Private Sub ComboBox1_SelectedIndexChanged_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
         If ComboBox1.SelectedIndex = 0 Then
             GroupBox1.Show()
             GroupBox2.Hide()
@@ -71,6 +85,49 @@ Public Class FormularioMatricula
     End Sub
 
     Private Sub Button2_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGuardar2.Click
+
+        If txtApePatAlumno.Text = "" Or txtApeMatAlumno.Text = "" And txtNombresAlumno.Text = "" And txtEdadAlumno.Text = "" _
+            And txtCalleAlumno.Text = "" And txtSectorAlumno.Text = "" Or txtCurso.Text = "" Or txtComunaAlumno.Text = "" _
+            Or txtTelefonoAlumno.Text = "" Or txtColegioPrese.Text = "" Or txtCursosRepetidos.Text = "" Then
+
+            MessageBox.Show("Debe ingresar todos los datos del alumno", "Datos de alumno", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Exit Sub
+        End If
+
+        While radioHermanosSi.Checked = True
+            If txtHermanosCursos.Text = "" Then
+                MessageBox.Show("Debe ingresar los cursos de hermanos", "Datos de alumno", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                Exit Sub
+            End If
+        End While
+
+        If txtNumHijos.Text = "" Or txtLugarHijos.Text = "" Or txtGrupoFamiliar.Text = "" Then
+            MessageBox.Show("Debe ingresar los datos de convivencia", "Datos de alumno", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Exit Sub
+        End If
+
+        While cbViveCon.Text = ""
+            MessageBox.Show("Debe seleccionar algun tipo de convivencia", "Datos de alumno", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Exit Sub
+            Exit While
+        End While
+       
+        While cbViveCon.SelectedIndex = "4"
+            If txtViveConOtros.Text = "" Then
+                MessageBox.Show("Debe ingresar otro tipo de conviviente", "Datos de alumno", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                Exit Sub
+            End If
+            Exit While
+        End While
+
+        ' While txtOtrosServicios.Enabled = True
+        ' If txtOtrosServicios.Text = "" Then
+        ' MessageBox.Show("Debe seleccionar un servicio de salud", "Datos de alumno", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        'Exit Sub
+        'End If
+        'End While
+
+
         indiceTab = TabControl1.SelectedIndex
         TabControl1.DeselectTab(indiceTab)
     End Sub
@@ -103,63 +160,6 @@ Public Class FormularioMatricula
         End If
     End Sub
 
-    Private Sub btnTerminar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnTerminar.Click
-
-        If RadioButton9.Checked = False And RadioButton13.Checked = False And RadioButton14.Checked = False Then
-
-            MessageBox.Show("Debe seleccionar uno de los tipos de tutor economico", "Matricula - Contrato de servicio", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
-            Exit Sub
-        End If
-
-        While RadioButton14.Checked = True
-            If txtOtro.Text = "" Then
-                MessageBox.Show("Debe ingresar otro tipo de tutor economico", "Matricula - Contrato de servicio", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
-                Exit Sub
-            End If
-
-        End While
-
-
-        If txtNombreTutor.Text = "" Or txtRut.Text = "" Or txtTelefonoPart.Text = "" Or txtTelefonoTrabajo.Text = "" Or txtDomicilio.Text = "" Or txtLugarDeTrabajo.Text = "" Or txtOcupacionAct.Text = "" Or txtProfesion.Text = "" Then
-
-            MessageBox.Show("Debe llenar todos los datos del tutor economico", "Matricula - Contrato de servicio", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
-            Exit Sub
-        End If
-
-        If radioMasc.Checked = False And radioFeme.Checked = False Then
-            MessageBox.Show("Debe seleccionar uno de los dos sexos", "Matricula - Ficha de matricula", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
-            Exit Sub
-        End If
-
-        If radioHermanosSi.Checked = False And radioHermanosNo.Checked = False Then
-            MessageBox.Show("Debe seleccionar hermanos en establecimiento", "Matricula - Ficha de matricula", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
-            Exit Sub
-        End If
-
-        While radioHermanosSi.Checked = True
-            If txtHermanosCursos.Text = "" Then
-                MessageBox.Show("Debe indicar los cursos de hermanos", "Matricula - Ficha de matricula", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
-                Exit Sub
-            End If
-        End While
-
-        If txtNombreApodSuplent.Text = "" Then
-            MessageBox.Show("Debe ingresar nombre de apoderado suplente", "Matricula - Ficha de matricula", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
-            Exit Sub
-        End If
-
-        '---------------------------------------------------------------------------------------------------
-        '---------------------------------------------------------------------------------------------------
-        '---------------------------------------------------------------------------------------------------
-        '---------------------------------------------------------------------------------------------------
-        '---------------------------------------------------------------------------------------------------
-        '---------------------------------------------------------------------------------------------------
-        '---------------------------------------------------------------------------------------------------
-
-       
-
-    End Sub
-
     Private Sub radioHermanosSi_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles radioHermanosSi.CheckedChanged
         If radioHermanosSi.Checked = True Then
             txtHermanosCursos.Enabled = True
@@ -171,17 +171,17 @@ Public Class FormularioMatricula
 
     Private Sub txtOtrosServicios_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
         If txtOtrosServicios.Text <> "" Then
-            comboIsapre.SelectedText = ""
+            comboServSalud.SelectedText = ""
         End If
     End Sub
 
     Private Sub comboIsapre_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
-        If comboIsapre.SelectedText <> "" Then
+        If comboServSalud.SelectedText <> "" Then
             txtOtrosServicios.Text = ""
         End If
     End Sub
 
-    Private Sub ComboBox14_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles cbViveCon.SelectedIndexChanged
+    Private Sub ComboBox14_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbViveCon.SelectedIndexChanged
         If cbViveCon.SelectedIndex = "4" Then
             txtViveConOtros.Enabled = True
             txtViveConOtros.Focus()
@@ -190,7 +190,7 @@ Public Class FormularioMatricula
         End If
     End Sub
 
-    Private Sub cbEstudiosMadre_SelectedIndexChanged(sender As System.Object, e As System.EventArgs)
+    Private Sub cbEstudiosMadre_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
         If cbEstudiosMadre.Text = "" Then
             txtEstudiosMadre.Enabled = False
         Else
@@ -199,7 +199,7 @@ Public Class FormularioMatricula
         End If
     End Sub
 
-    Private Sub cbEstudiosPadre_SelectedIndexChanged(sender As System.Object, e As System.EventArgs)
+    Private Sub cbEstudiosPadre_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
         If cbEstudiosPadre.Text = "" Then
             txtEstudiosPadre.Enabled = False
         Else
@@ -208,7 +208,7 @@ Public Class FormularioMatricula
         End If
     End Sub
 
-    Private Sub ComboBox12_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles cbApoderado.SelectedIndexChanged
+    Private Sub ComboBox12_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbApoderado.SelectedIndexChanged
         If cbApoderado.SelectedIndex = "3" Then
             txtNombreApoderado.Enabled = True
             txtNombreApoderado.Focus()
@@ -217,7 +217,7 @@ Public Class FormularioMatricula
         End If
     End Sub
 
-    Private Sub ComboBox13_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles cbApoSuplente.SelectedIndexChanged
+    Private Sub ComboBox13_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbApoSuplente.SelectedIndexChanged
         If cbApoSuplente.SelectedIndex = "3" Then
             txtNombreApodSuplent.Enabled = True
             txtNombreApodSuplent.Focus()
@@ -235,5 +235,13 @@ Public Class FormularioMatricula
     Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         indiceTab = TabControl1.SelectedIndex
         TabControl1.SelectTab(indiceTab - 1)
+    End Sub
+
+    Private Sub txtOtrosServicios_TextChanged_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtOtrosServicios.TextChanged
+        If txtOtrosServicios.Text <> "" Then
+            comboServSalud.Enabled = False
+        ElseIf txtOtrosServicios.Text = "" Then
+            comboServSalud.Enabled = True
+        End If
     End Sub
 End Class
---
+++ ColegioEchaurren/ColegioEchaurren/ModuloContenedor.vb
@@ -0,0 +1,3 @@
+﻿Module ModuloContenedor
+
+End Module
